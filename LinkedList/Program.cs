80f2fd0574d9c9d3a74c02274384296971ac8147
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-09 22:19:45 +0530

    [Soubarnika]UC1-Creating Simple Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -11,6 +11,16 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Implementaion of Linked List");
+            LinkedList<int> linkedList = new LinkedList<int>();
+           
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            foreach (int i in linkedList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
         }
     }
 }
